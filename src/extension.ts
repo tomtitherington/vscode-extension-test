6dfe55a07207ae973602d0c7956024f3b550fa66
tomtitherington <tom.titherington@gmail.com>
2022-08-25 14:52:06 +0100

    Added 'newTag' command, activated on startup and executed on context menu click.

--- src/extension.ts
+++ src/extension.ts
@@ -5,22 +5,28 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-test-extension" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vscode-test-extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Test Extension!');
-	});
+  let disposables: vscode.Disposable[] = [];
 
-	context.subscriptions.push(disposable);
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "vscode-test-extension" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  disposables.push(vscode.commands.registerCommand('vscode-test-extension.helloWorld', () => {
+    // The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Hello World from Test Extension!');
+  }));
+
+  disposables.push(vscode.commands.registerCommand('vscode-test-extension.newTag', () => {
+    vscode.window.showInformationMessage('You have right clicked.');
+  }));
+
+  context.subscriptions.push(...disposables);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
